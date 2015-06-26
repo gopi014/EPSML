5465df4d06c8815ef18cbe28b0cf33c7eca1ed58
gopi014 <gopiforroses@gmail.com>
2015-06-26 14:36:04 +0530

    name changed to thinkmatix

--- apps/EPSML/android/native/assets/www/default/worklight/checksum.js
+++
@@ -1,2 +0,0 @@
-var WL_CHECKSUM = {"checksum":2514064195,"date":1434375992114,"machine":"ADMINIB-J5FNIM5"};
-/* Date: Mon Jun 15 19:16:32 IST 2015 */
---
+++ apps/Thinkmatix/android/native/assets/www/default/worklight/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"checksum":1536860502,"date":1435304555493,"machine":"ADMINIB-J5FNIM5"};
+/* Date: Fri Jun 26 13:12:35 IST 2015 */
